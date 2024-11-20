cde175283accbd7ac2d164d960849b7f8e775673
orca <edoch06@gmail.com>
2024-11-20 13:08:49 +0300

    added CancelGiftRegistry

---
+++ PointOfSale/CancelGiftRegistry.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CancelGiftRegistry
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.panelheader = New System.Windows.Forms.Panel
+        Me.btn_ok = New System.Windows.Forms.Button
+        Me.exitBtn = New System.Windows.Forms.Button
+        Me.POSTitleLbl = New System.Windows.Forms.Label
+        Me.panelheader.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panelheader
+        '
+        Me.panelheader.BackColor = System.Drawing.Color.White
+        Me.panelheader.Controls.Add(Me.btn_ok)
+        Me.panelheader.Controls.Add(Me.exitBtn)
+        Me.panelheader.Controls.Add(Me.POSTitleLbl)
+        Me.panelheader.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.panelheader.Location = New System.Drawing.Point(2, 2)
+        Me.panelheader.Name = "panelheader"
+        Me.panelheader.Size = New System.Drawing.Size(334, 110)
+        Me.panelheader.TabIndex = 296
+        '
+        'btn_ok
+        '
+        Me.btn_ok.BackColor = System.Drawing.Color.Gainsboro
+        Me.btn_ok.FlatAppearance.BorderColor = System.Drawing.Color.DimGray
+        Me.btn_ok.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightCoral
+        Me.btn_ok.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Tomato
+        Me.btn_ok.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_ok.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_ok.ForeColor = System.Drawing.Color.Black
+        Me.btn_ok.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btn_ok.Location = New System.Drawing.Point(115, 62)
+        Me.btn_ok.Name = "btn_ok"
+        Me.btn_ok.Size = New System.Drawing.Size(104, 28)
+        Me.btn_ok.TabIndex = 325
+        Me.btn_ok.Text = "OK"
+        Me.btn_ok.UseVisualStyleBackColor = False
+        '
+        'exitBtn
+        '
+        Me.exitBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.exitBtn.BackColor = System.Drawing.Color.White
+        Me.exitBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.exitBtn.FlatAppearance.BorderColor = System.Drawing.Color.DarkOliveGreen
+        Me.exitBtn.FlatAppearance.BorderSize = 0
+        Me.exitBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Brown
+        Me.exitBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.IndianRed
+        Me.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.exitBtn.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.exitBtn.ForeColor = System.Drawing.Color.Black
+        Me.exitBtn.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.exitBtn.Location = New System.Drawing.Point(303, 1)
+        Me.exitBtn.Name = "exitBtn"
+        Me.exitBtn.Size = New System.Drawing.Size(26, 22)
+        Me.exitBtn.TabIndex = 294
+        Me.exitBtn.Text = "X"
+        Me.exitBtn.UseVisualStyleBackColor = False
+        '
+        'POSTitleLbl
+        '
+        Me.POSTitleLbl.AutoSize = True
+        Me.POSTitleLbl.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.POSTitleLbl.ForeColor = System.Drawing.Color.Black
+        Me.POSTitleLbl.Location = New System.Drawing.Point(65, 26)
+        Me.POSTitleLbl.Name = "POSTitleLbl"
+        Me.POSTitleLbl.Size = New System.Drawing.Size(209, 24)
+        Me.POSTitleLbl.TabIndex = 296
+        Me.POSTitleLbl.Text = "Cancel Gift Registry"
+        '
+        'CancelGiftRegistry
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(338, 114)
+        Me.Controls.Add(Me.panelheader)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.KeyPreview = True
+        Me.Name = "CancelGiftRegistry"
+        Me.Padding = New System.Windows.Forms.Padding(2)
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CancelGiftRegistry"
+        Me.panelheader.ResumeLayout(False)
+        Me.panelheader.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents panelheader As System.Windows.Forms.Panel
+    Friend WithEvents exitBtn As System.Windows.Forms.Button
+    Friend WithEvents POSTitleLbl As System.Windows.Forms.Label
+    Friend WithEvents btn_ok As System.Windows.Forms.Button
+End Class
---
+++ PointOfSale/CancelGiftRegistry.vb
@@ -0,0 +1,30 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.Common
+Imports System.Data.Odbc
+Imports System.Data.SqlClient
+Imports System.Drawing.Printing
+Imports System.IO
+Imports System.Text
+Imports PointOfSale.ClassConnections
+Public Class CancelGiftRegistry
+
+
+    Dim dAdapt, da As Odbc.OdbcDataAdapter
+    Dim dSet As DataSet
+    Dim ds As New DataSet
+    Dim a As Integer = 0
+    Dim dBind As New BindingSource
+    Dim classconn As New ClassConnections
+    Dim classPos As New ClassPOS
+    Dim mysqlConn As String = classconn.GetServerVariables()
+    Dim ConnSqlStr As String = classconn.GetSqlConnStr()
+    Public Shared formCode As String
+    Dim change As Double = SalesRefunds.txt_balance.Text
+    Private IsFormBeingDragged As Boolean = False
+    Private MouseDownX As Integer
+    Private MouseDownY As Integer
+    Private UpdateCode As Integer
+
+   
+End Class
