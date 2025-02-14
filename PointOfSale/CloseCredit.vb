782f04606d672c8b8e291d6bcfe2df67315de3ce
orca <edoch06@gmail.com>
2025-02-14 14:45:12 +0300

    added CloseCredit

---
+++ PointOfSale/CloseCredit.vb
@@ -0,0 +1,14 @@
+﻿
+Imports System
+Imports System.Data
+Imports System.Data.Common
+Imports System.Data.Odbc
+Imports System.Data.SqlClient
+Imports System.Drawing.Printing
+Imports System.IO
+Imports System.Text
+Imports PointOfSale.ClassConnections
+Public Class CloseCredit
+
+
+End Class
